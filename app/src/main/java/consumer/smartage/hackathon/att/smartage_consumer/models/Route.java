17ba4f4d0d366302f05b9bcb9034f459c0fa9a7e
frizzbolt <joshua.a.abrams@gmail.com>
2016-06-11 13:34:30 -0500

    Fix implementation based on comments

--- app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/models/Route.java
+++ app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/models/Route.java
@@ -11,15 +11,20 @@ import android.view.View;
  * Created by joshabrams on 6/11/16.
  */
 public class Route {
-    private Trashcan[] waypoints;
+    private Arraylist<Trashcan> waypoints;
 
     public Route() {
-        this.waypoints = new Trashcan[2];
+        this.waypoints = new Arraylist<Trashcan>();
     }
 
-    public void plotRoute() {
+    public Intent plotRoute() {
+        String startingLatitude = waypoints.get(0).getLatitude.ToString();
+        String startingLongitude = waypoints.get(0).getLongitude.ToString();
+        String destinationLatitude = waypoints.get(1).getLatitude.ToString();
+        String destinationLongitude = waypoints.get(1).getLongitude.ToString();
+
         Intent intent = new Intent(android.content.Intent.ACTION_VIEW,
-                Uri.parse("http://maps.google.com/maps?saddr=" + waypoints[0].latitude + "," + waypoints[0].longitude + "&daddr=" + waypoints[1].latitude + "," + waypoints[1].longitude));
-        startActivity(intent);
+                Uri.parse("http://maps.google.com/maps?saddr=" + startingLatitude + "," + startingLongitude + "&daddr=" + destinationLatitude + "," + destinationLongitude));
+        return intent;
     }
 }
