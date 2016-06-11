bc227c9ef423df83451dcac20b925443308d5b56
Jonathan Morton <djlarrikin@gmail.com>
2016-06-11 10:46:47 -0500

    Initial work on maps

--- app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/fragments/MapConsumerFragment.java
+++ app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/fragments/MapConsumerFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.SupportMapFragment;
+
 import consumer.smartage.hackathon.att.smartage_consumer.R;
 
 /**
@@ -18,7 +20,7 @@ import consumer.smartage.hackathon.att.smartage_consumer.R;
  * Use the {@link MapConsumerFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class MapConsumerFragment extends Fragment {
+public class MapConsumerFragment extends SupportMapFragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
