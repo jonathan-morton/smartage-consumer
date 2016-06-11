b25ca599bda164b926508b96bd63731e74633dc0
Jonathan Morton <djlarrikin@gmail.com>
2016-06-10 23:21:17 -0500

    Initial setup of activities

---
+++ app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/activities/MainConsumerActivity.java
@@ -0,0 +1,15 @@
+package consumer.smartage.hackathon.att.smartage_consumer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import consumer.smartage.hackathon.att.smartage_consumer.R;
+
+public class MainConsumerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_consumer);
+    }
+}
---
+++ app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/activities/MainDriverActivity.java
@@ -0,0 +1,12 @@
+package consumer.smartage.hackathon.att.smartage_consumer.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainDriverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
