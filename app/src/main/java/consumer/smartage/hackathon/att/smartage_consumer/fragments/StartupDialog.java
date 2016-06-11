a840df3e6c9f7e1efbe9e2f87c6c807231684f94
rushit <rushit_thakker@yahoo.com>
2016-06-11 17:12:58 -0500

    Adding dialog 1

--- app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/fragments/StartupDialog.java
+++ app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/fragments/StartupDialog.java
@@ -3,25 +3,41 @@ package consumer.smartage.hackathon.att.smartage_consumer.fragments;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import consumer.smartage.hackathon.att.smartage_consumer.R;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class StartupDialog extends android.app.DialogFragment {
+public class StartupDialog extends DialogFragment {
 
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         AlertDialog.Builder builder =new AlertDialog.Builder(getActivity());
         builder.setTitle("Choose user type")
-                .setMessage("Who are you?");
+                .setMessage("Who are you?")
+                .setPositiveButton("Drive", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "You have selected drive mode", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setNegativeButton("User", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(getActivity(), "You have selected drive mode", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
         return builder.create();
     }
 }
--- app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/models/Route.java
+++ app/src/main/java/consumer/smartage/hackathon/att/smartage_consumer/models/Route.java
@@ -7,17 +7,23 @@ import android.os.Bundle;
 import android.app.Activity;
 import android.view.View;
 
+import java.util.ArrayList;
+
 /**
  * Created by joshabrams on 6/11/16.
  */
+/*
 public class Route {
-    private Arraylist<Trashcan> waypoints;
+    private ArrayList<Trashcan> waypoints;
 
     public Route() {
-        this.waypoints = new Arraylist<Trashcan>();
+        this.waypoints = new ArrayList<Trashcan>();
+        waypoints.add(1,new Trashcan());
+        waypoints.add(1,new Trashcan());
     }
 
     public Intent plotRoute() {
+
         String startingLatitude = waypoints.get(0).getLatitude.ToString();
         String startingLongitude = waypoints.get(0).getLongitude.ToString();
         String destinationLatitude = waypoints.get(1).getLatitude.ToString();
@@ -28,3 +34,4 @@ public class Route {
         return intent;
     }
 }
+*/
